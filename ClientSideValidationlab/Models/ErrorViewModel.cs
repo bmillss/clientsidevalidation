e05f973d62018be26d685cc2dbc98d7390876d1a
Brandon Miller <Brandonmiller1@quickenloans.com>
2021-10-05 09:06:36 -0400

    init commit

---
+++ ClientSideValidationlab/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ClientSideValidationlab.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ClientSideValidationlab/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientSideValidationlab.Models
+{
+    public class User
+    {
+        public string FirstName;
+        public string LastName;
+        public string Email;
+        public string Password;
+    }
+}
